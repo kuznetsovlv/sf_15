73f0a5ecbe125b68a93766af5f5ef42f0e0c13de
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2022-02-07 23:35:42 +0300

    Coppied original code

---
+++ chat.cpp
@@ -0,0 +1,15 @@
+#include "chat.h"
+#include "iostream"
+#include "string.h"
+
+Chat::Chat() {
+    data_count = 0;
+}
+void Chat::reg(char _login[LOGINLENGTH], char _pass[], int pass_length) {
+    // Ваш код
+    
+}
+bool Chat::login(char _login[LOGINLENGTH], char _pass[], int pass_length) {
+    // Ваш код
+    // Нужно вернуть true в случае успешного логина
+}
