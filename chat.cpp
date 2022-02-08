35fc339a96c5f609c6faf85b1c9c7689bf964f0a
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2022-02-08 23:02:42 +0300

    Finished chat

--- chat.cpp
+++ chat.cpp
@@ -1,17 +1,25 @@
-#include <iostream>
 #include <cstring>
 #include "chat.h"
 #include "hashTable.h"
+#include "sha1.h"
 
-Chat::Chat() {
-    data_count = 0;
-}
 void Chat::reg(char _login[LOGINLENGTH], char _pass[], int pass_length) {
     // Ваш код
-    
+    const uint *hash = sha1(_pass, pass_length);
+
+	_data.add(_login, hash);
+	delete[] hash;
 }
 bool Chat::login(char _login[LOGINLENGTH], char _pass[], int pass_length) {
     // Ваш код
     // Нужно вернуть true в случае успешного логина
+	const uint *saved = _data.find(_login);
+	if(saved)
+	{
+		const uint *current = sha1(_pass, pass_length);
+		bool logged = !memcmp(saved, current, SHA1HASHLENGTHBYTES);
+		delete[]current;
+		return logged;
+	}
     return false;
 }
